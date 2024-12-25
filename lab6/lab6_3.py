604e2ee54e05ea3644ffa6b453dbb2d8113d2580
Denis-gr1 <dengrishaew1005@gmail.com>
2024-12-25 16:13:36 +0500

    Update lab6_3.py

--- lab6/lab6_3.py
+++ lab6/lab6_3.py
@@ -1,1 +1,28 @@
+def is_odd_prime(num):
+    if num < 2:
+        return False
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def find_numbers(start, end):
+    results = []
+    for p in range(3, int(end**0.25) + 1, 2):
+        if is_odd_prime(p):
+            n = p ** 4
+            if start <= n <= end:
+                results.append(n)
+    return results
+
+start_range = 45000000
+end_range = 50000000
+numbers_with_five_odd_divisors = find_numbers(start_range, end_range)
+
+numbers_with_five_odd_divisors.sort()
+
+for number in numbers_with_five_odd_divisors:
+    print(number)
 
